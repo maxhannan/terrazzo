af5ff37e1873c470e4e947b408c89cceae4c4e02
maxhannan <maxhannan@substanceseven.com>
2021-02-14 02:14:50 -0600

    about page initial structure

--- src/about.js
+++ src/about.js
@@ -1,7 +1,22 @@
-import {heroFactory} from './heroConstructor'
+import { Loader } from "@googlemaps/js-api-loader"
 const aboutFactory = (content) =>{
-    let hero = heroFactory('about').hero;
-    content.appendChild(hero) 
+    let map = document.createElement('div');
+    map.id = 'map'
+    
+    content.appendChild(map)
+    let aboutContainer = document.createElement('div');
+    aboutContainer.classList.add('aboutContainer');
+    content.appendChild(aboutContainer)
+    const loader = new Loader({
+        apiKey: "AIzaSyAzvI9cu1Zk8OegSCLn8Yv0BTagQSWDMFQ",
+        version: "weekly",
+      });
+      loader.load().then(() => {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      }); 
 }
 export{
     aboutFactory
